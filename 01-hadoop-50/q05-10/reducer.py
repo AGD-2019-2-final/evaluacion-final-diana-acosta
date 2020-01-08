f8fcaa0d7a5bf4acad6b009a0e0ddf86d9ad2002
diana-acosta <diacosta@unal.edu.co>
2020-01-07 22:14:46 -0500

    Update reducer.py

--- 01-hadoop-50/q05-10/reducer.py
+++ 01-hadoop-50/q05-10/reducer.py
@@ -2,4 +2,17 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+currentk = None
+count = 0
 
+for line in sys.stdin:
+    line = line.strip()
+    if line == currentk:
+        count += 1
+    else:
+        if currentk is not None:
+            print(currentk + "\t" + str(count))
+        currentk = line
+        count = 1
+        
+print(currentk + "\t" + str(count))
