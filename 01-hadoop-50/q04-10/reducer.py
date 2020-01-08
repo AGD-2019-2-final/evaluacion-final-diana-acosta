80abb93cbf632ed93f0db76a9cf2971d26bb4b48
diana-acosta <diacosta@unal.edu.co>
2020-01-07 22:14:39 -0500

    Update reducer.py

--- 01-hadoop-50/q04-10/reducer.py
+++ 01-hadoop-50/q04-10/reducer.py
@@ -2,4 +2,17 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+current_letter = None
+count = 0
 
+for line in sys.stdin:
+    line = line.strip()
+    if line == current_letter:
+        count += 1
+    else:
+        if current_letter is not None:
+            print(current_letter + "," + str(count))
+        current_letter = line
+        count = 1
+        
+print(current_letter + "," + str(count))
