886df1a8418b1ff81d2dc98ca7d3ae6d1d8c0373
diana-acosta <diacosta@unal.edu.co>
2020-01-07 22:14:42 -0500

    Update mapper.py

--- 01-hadoop-50/q05-10/mapper.py
+++ 01-hadoop-50/q05-10/mapper.py
@@ -2,3 +2,10 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+for line in sys.stdin:
+    col2 = ""
+    line = line.strip()
+    splits = line.split("   ")
+    col2 = splits[1]
+    col2 = col2[5:7]
+    print(col2)
