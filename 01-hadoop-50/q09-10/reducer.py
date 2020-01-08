6e13b37e6b6ec3598b2e76e4eec4bc6bc5a435bb
diana-acosta <diacosta@unal.edu.co>
2020-01-07 22:15:13 -0500

    Update reducer.py

--- 01-hadoop-50/q09-10/reducer.py
+++ 01-hadoop-50/q09-10/reducer.py
@@ -2,3 +2,11 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+count = [1, 2, 3, 4, 5, 6, 7]
+
+for line in sys.stdin:
+    line = line.strip()
+    n, d, l = line.split("\t")
+    n = int(n)
+    if n in count:
+        print(l + "\t" + d + "\t" + str(n))
