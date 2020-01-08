4aa8ff35107a0c2186b3cac4e970513de8f2732a
diana-acosta <diacosta@unal.edu.co>
2020-01-07 22:15:06 -0500

    Update reducer.py

--- 01-hadoop-50/q08-10/reducer.py
+++ 01-hadoop-50/q08-10/reducer.py
@@ -2,3 +2,22 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+currentk = None
+vsum = 0
+vcount= 0
+
+for line in sys.stdin:
+    line = line.strip()
+    k, v = line.split("\t")
+    v = int(v)
+    if k == currentk:
+        vsum += v
+        vcount += 1
+    else:
+        if currentk is not None:
+            print(currentk + "\t" + str(float(vsum)) + "\t" + str(vsum/vcount))
+        currentk = k
+        vsum = v
+        vcount = 1
+        
+print(currentk + "\t" + str(float(vsum)) + "\t" + str(vsum/vcount))
